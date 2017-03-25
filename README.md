65ac47e6809920b60d5002bc6a6006ebc472c176
lxynox <lxynox@gmail.com>
2017-03-25 01:09:51 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# mac-navbar-clone
-➠ neat navbar
+# mac-dock-clone
+🎱 neat, minimalistic, customizable react component (inspired by mac dock app)
+
+### feature
+- `mouseover`, `mouseout` to zoom/un-zoom icons
+- `click` to change _active_ icon
+- customizable icons, zoom factors, positions etc
+
+### usage
+
+| default props | value |
+| :------------- | :------------- |
+| children (nav items) | `['🤣', '🙃', '😞', '🙁', '🤔'].map(e => <span>{e}</span>)` |
+| pos (position of dock left/right/bottom) | 'bottom' |
+| zooms (factors to zoom the hovered icons) | [2, 1.5, 1.2] |
+| style (customizable styles) | { active: {border: '5px outset rgba(142, 139, 140, 0.8)'} } |
+
+```js
+import Dock from './dist/bundle.js'
+
+const YourWrapperComponent = () => (
+	<div>
+		...
+		<Dock />
+		...
+	</div>
+)
+```
+
+### preview
+![preview](./preview)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const webpack = require('webpack')
 
 module.exports = {
 	entry: './lib/main.js',
 	output: {
 		publicPath: '/dist',
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		library: 'react-macdock',
+		libraryTarget: 'umd'
 	},
 	module: {
 		rules: [
@@ -21,4 +24,25 @@ module.exports = {
 		noInfo: true
 	},
 	devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+	module.exports.devtool = '#source-map'
+
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	])
 }
